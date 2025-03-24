5a66739a1f697fa89a59b14f53a79811ecd41c84
a.awasthi <a.awasthi@techademy.com>
2025-03-25 01:05:48 +0530

    host changed, and env added

--- tests/TestResults.py
+++ tests/TestResults.py
@@ -1,7 +1,12 @@
 import json
+
+
 class TestResults(dict):
     testCaseResults = ""
     customData = ""
+    hostName = ""
+    attemptId = ""
 
-    def __init__(self, testCaseResults, customData):
-        dict.__init__(self, testCaseResults = testCaseResults, customData = customData)
+    def __init__(self, testCaseResults, customData, hostName, attemptId):
+        dict.__init__(self, testCaseResults=testCaseResults, customData=customData, hostName=hostName,
+                      attemptId=attemptId)
--- tests/TestUtils.py
+++ tests/TestUtils.py
@@ -1,11 +1,13 @@
-from tests.TestResults import TestResults
-from tests.TestCaseResultDto import TestCaseResultDto
+from test.TestResults import TestResults
+from test.TestCaseResultDto import TestCaseResultDto
 import json
 import requests
+import os
 
 class TestUtils:
     GUID = "dc66f3c1-630f-40ab-8314-f7bb9ffcb71f"
-    URL = "https://yaksha-prod-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=jSTWTxtQ8kZgQ5FC0oLgoSgZG7UoU9Asnmxgp6hLLvYId/GW9ccoLw=="
+    # URL = "https://yaksha-prod-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=jSTWTxtQ8kZgQ5FC0oLgoSgZG7UoU9Asnmxgp6hLLvYId/GW9ccoLw=="
+    URL = "https://compiler.techademy.com/v1/mfa-results/push"
 
     @classmethod
     def yakshaAssert(self, test_name, result, test_type):
@@ -23,8 +25,14 @@ class TestUtils:
         test_case_result_dto = TestCaseResultDto(test_name, test_type, 1, result_score, result_status, True, "")
         test_case_results[self.GUID] = test_case_result_dto
 
-        test_results = TestResults(json.dumps(test_case_results), customData)
+        hostName = os.environ.get('HOSTNAME')
+        attemptId = os.environ.get('ATTEMPT_ID')
+
+        test_results = TestResults(json.dumps(test_case_results), customData, hostName, attemptId)
 
         final_result = json.dumps(test_results)
 
-        requests.post(self.URL, final_result)
+        response = requests.post(self.URL, final_result, headers={"Content-Type": "application/json"})
+        if response.status_code not in [200, 201]:
+            length = len(customData)
+            print(f'⚠️ Unable to push test cases from {hostName}, please try again![{length}]')
